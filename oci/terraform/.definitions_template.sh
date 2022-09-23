b8e18c985b4ea63c6d0d036c8c2622b621cc7c31
dodopontocom <rodolfotiago@gmail.com>
2022-09-22 22:39:54 -0300

    template to help in definitions file

---
+++ oci/terraform/.definitions_template.sh
@@ -0,0 +1,11 @@
+# steps -> 
+### 1:
+# $ mv .definitions_template.sh .definitions.sh
+### 2:
+# fill in the variable values (add more if necessary)
+### 3:
+# $ source .definitions.sh
+export TF_VAR_region="sa-vinhedo-1"
+export TF_VAR_tenancy_ocid=""
+export TF_VAR_user_ocid=""
+export TF_VAR_vcn_name="my-vcn"
